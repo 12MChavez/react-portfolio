2c98a0c6cbfd50d98868472c01e52ce4dee65aec
Melissa <mel.chavez.code@gmail.com>
2023-01-12 13:57:52 -0500

    update validation for name and textarea in contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,29 +4,67 @@ import { Container, Form, Button } from "react-bulma-components";
 import FooterC from "../components/Footer";
 import validator from "validator";
 
+let nameStatus = false;
 let emailValue;
-function setEmailStatus(evt) {
-  let newEmailValue = evt.target.value;
+let emailStatus = false;
+
+let msgStatus = false;
+
+const setNameAdded = (evt) => {
+  let name = evt.target.value;
+
+  if (name.length > 0) {
+    console.log("true");
+    nameStatus = true;
+  } else {
+    nameStatus = false;
+  }
+};
+
+const setEmailStatus = (evt) => {
+  emailValue = evt.target.value;
+
+  if (validator.isEmail(emailValue)) {
+    console.log("true");
+    emailStatus = true;
+  } else {
+    emailStatus = false;
+  }
+};
+
+const setMessageAdded = (evt) => {
+  let message = evt.target.value;
+
+  if (message.length > 0) {
+    console.log("true");
+    msgStatus = true;
+  } else {
+    msgStatus = false;
+  }
+};
+
+function showSubmit() {
   const submitBtn = document.getElementById("submitBtn");
-
-  let isValidEmail;
-
-  if (validator.isEmail(newEmailValue)) {
+  console.log(emailStatus, msgStatus, nameStatus);
+  if (emailStatus === true && msgStatus === true && nameStatus === true) {
     submitBtn.classList.remove("is-hidden");
-    isValidEmail = true;
+    console.log("name, ValidEmail and message added");
   } else {
     submitBtn.classList.add("is-hidden");
-    isValidEmail = false;
+    console.log("fill out form");
   }
-  console.log(isValidEmail);
 }
 const Contact = (
   <div>
-    <Container>
+    <Container onChange={() => showSubmit()}>
       <Form.Field>
         <Form.Label>Name: </Form.Label>
         <Form.Control>
-          <Form.Input color="success" placeholder="Name" />
+          <Form.Input
+            color="success"
+            placeholder="Name"
+            onChange={(evt) => setNameAdded(evt)}
+          />
           <Form.Label>Email: </Form.Label>
           <Form.Help color="danger">
             Submit button will show when email entered
@@ -40,7 +78,11 @@ const Contact = (
             onChange={(evt) => setEmailStatus(evt)}
           />
           <Form.Label>Message: </Form.Label>
-          <Form.Textarea color="success" placeholder="Message" />
+          <Form.Textarea
+            color="success"
+            placeholder="Message"
+            onChange={(evt) => setMessageAdded(evt)}
+          />
           <Button
             type="submit"
             color="link"
