74e896c12c16a5ffb53e9cdb20583a5cf93a5e8d
Melissa <mel.chavez.code@gmail.com>
2023-01-16 20:31:18 -0500

    update Resume format

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -13,47 +13,28 @@ import {
 const Resume = (
   <div>
     <Columns>
-      <Columns.Column offset={1} size={4}>
-        <Heading size={5}>Skills: </Heading>
-      </Columns.Column>
+      <Columns.Column offset={1} size={4}></Columns.Column>
     </Columns>
     <Columns>
       <Columns.Column size={4} offset={1}>
         <Notification color="primary">
+          <Heading size={5}>Skills: </Heading>
           <ul>
             <li>HTML</li>
             <li>CSS</li>
             <li>JavaScript</li>
             <li>Liquid</li>
             <li>jQuery</li>
-          </ul>
-        </Notification>
-      </Columns.Column>
-      <Columns.Column size={4} offset={1}>
-        <Notification color="primary">
-          <ul>
             <li>Bootstrap</li>
             <li>Bulma</li>
             <li>Git</li>
             <li>Python</li>
             <li>Node.js</li>
-          </ul>
-        </Notification>
-      </Columns.Column>
-      <Columns.Column size={4} offset={1}>
-        <Notification color="primary">
-          <ul>
             <li>Express.js</li>
             <li>mysql</li>
             <li>Sequelize</li>
             <li>NoSQL</li>
             <li>MongoDB</li>
-          </ul>
-        </Notification>
-      </Columns.Column>
-      <Columns.Column size={4} offset={1}>
-        <Notification color="primary">
-          <ul>
             <li>Insomnia</li>
             <li>TDD</li>
             <li>React.js</li>
@@ -61,23 +42,23 @@ const Resume = (
           </ul>
         </Notification>
       </Columns.Column>
-    </Columns>
-
-    <Columns>
-      <Columns.Column offset={4} size={4}>
-        <Box>
-          <a href={require("../assets/resume.pdf")} download>
-            <Block>
-              <Image
-                alt="resume-image"
-                src={require("../assets/images/resume.jpg")}
-              />
-            </Block>
-          </a>
-          <Block>Click image to download a copy of my resume.</Block>
-        </Box>
+      <Columns.Column size={4} offset={1}>
+        <Notification color="primary">
+          <Box>
+            <a href={require("../assets/resume.pdf")} download>
+              <Block>
+                <Image
+                  alt="resume-image"
+                  src={require("../assets/images/resume.jpg")}
+                />
+              </Block>
+            </a>
+            <Block>Click image to download a copy of my resume.</Block>
+          </Box>
+        </Notification>
       </Columns.Column>
     </Columns>
+
     <FooterC />
   </div>
 );
